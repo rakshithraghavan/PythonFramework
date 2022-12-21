00778c7799de69dc7ce50e54ec45e5db82b3c116
rakshithraghavan <71182349+rakshithraghavan@users.noreply.github.com>
2022-12-21 15:37:53 +0530

    Update conftest.py
    # _capture_screenshot

--- tests/conftest.py
+++ tests/conftest.py
@@ -52,9 +52,9 @@ def pytest_runtest_makereport(item):
 
     if report.when == 'call' or report.when == "setup":
         xfail = hasattr(report, 'wasxfail')
-        if (report.skipped and xfail) or (report.failed and not xfail):
-            file_name = report.nodeid.replace("::", "_") + ".png"
-            _capture_screenshot(file_name)
+#        if (report.skipped and xfail) or (report.failed and not xfail):
+#            file_name = report.nodeid.replace("::", "_") + ".png"
+#            _capture_screenshot(file_name)
             if file_name:
                 html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
                        'onclick="window.open(this.src)" align="right"/></div>' % file_name
